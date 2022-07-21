06fb8ea13567e00cbb57a12899fc5d24bcf39e56
metalveor <metalveor@gmail.com>
2022-07-21 23:07:57 +0300

    Add HW_3_task_3

--- vajda_maksim_valerevich/hm_3/home_work_3.py
+++ vajda_maksim_valerevich/hm_3/home_work_3.py
@@ -14,3 +14,13 @@ word_3 = 'jump'
 add_ing = 'ing'
 
 print('task_2: ' + word_1 + add_ing, word_2 + add_ing, word_3 + add_ing)
+
+
+# 3. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+
+name = 'Ivanou Ivan'
+
+first_name = name[name.find(' ')+1:]
+last_name = name[:name.find(' ')+1]
+
+print('task_2: ' + first_name + ' ' + last_name)
