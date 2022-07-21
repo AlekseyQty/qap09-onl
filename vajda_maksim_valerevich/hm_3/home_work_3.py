35cc7ab8206437812f473ab94f72994d8c8640d8
metalveor <metalveor@gmail.com>
2022-07-21 22:56:33 +0300

    Add HW_3_task_2

--- vajda_maksim_valerevich/hm_3/home_work_3.py
+++ vajda_maksim_valerevich/hm_3/home_work_3.py
@@ -2,6 +2,15 @@
 
 add_str = 'www.my_site.com#about'
 
-print(add_str.replace('#', '/'))
+print('task_1: ' + add_str.replace('#', '/'))
 
 
+# 2. Напишите программу, которая добавляет ‘ing’ к словам
+
+word_1 = 'play'
+word_2 = 'drink'
+word_3 = 'jump'
+
+add_ing = 'ing'
+
+print('task_2: ' + word_1 + add_ing, word_2 + add_ing, word_3 + add_ing)
