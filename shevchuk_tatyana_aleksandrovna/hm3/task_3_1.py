d7cf063b9c9960d1657fad7a40b6cbe4b537a02b
Tatyana Shevchuk <shevtanya2022@gmail.com>
2022-07-22 19:30:03 +0200

    add files with hm3

---
+++ shevchuk_tatyana_aleksandrovna/hm3/task_3_1.py
@@ -0,0 +1,6 @@
+# Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+
+
+string_url = "www.my_site.com#about"
+print(f'Original version: {string_url}')
+print(f'Modified version: {string_url.replace("#", "/")}')
---
+++ shevchuk_tatyana_aleksandrovna/hm3/task_3_3.py
@@ -0,0 +1,4 @@
+# В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+
+surname_name = "Ivanou Ivan"
+print(f'Ivanou Ivan - {surname_name[7:] + " " + surname_name[:6]}')
