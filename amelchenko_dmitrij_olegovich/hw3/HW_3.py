dde9b5924a919858b5236751e26edb705235a0a7
AmelchenkoDima <Amelchencko.dima1998@gmail.com>
2022-07-24 00:39:50 +0300

    add tasks 2 - 4

--- amelchenko_dmitrij_olegovich/hw3/HW_3.py
+++ amelchenko_dmitrij_olegovich/hw3/HW_3.py
@@ -5,4 +5,22 @@ print(str.replace('#', '/'))
 
 #2. Напишите программу, которая добавляет ‘ing’ к словам
 
+wd_1 = 'Read'
+wd_2 = 'Go'
+
+add_ing = 'ing'
+print(wd_1+add_ing, wd_2+add_ing)
+
+#3.В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+
+last_name = 'Ivanou'
+first_name = 'Ivan'
+
+print(f'{first_name} {last_name}')
+
+#4. Напишите программу которая удаляет пробел в начале, в конце строки
+
+task_4 = '   Hello world   '
+
+print(task_4.strip())
 
