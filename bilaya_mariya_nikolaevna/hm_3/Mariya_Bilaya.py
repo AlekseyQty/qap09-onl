4d9012f40750e6d2cd8efbf6178ffcfb4ad14f75
Mary Bilaya <marybilaya@gmail.com>
2022-07-25 15:34:05 +0300

    Complete hm_3

--- bilaya_mariya_nikolaevna/hm_3/Mariya_Bilaya.py
+++ bilaya_mariya_nikolaevna/hm_3/Mariya_Bilaya.py
@@ -1,31 +1,28 @@
 # Exercise_1. Заменить символ "#" на символ "/" в строке 'www.my_site.com#about'
 
-
 my_str = "www.my_site.com#about"
 my_str1 = (my_str.replace("#", "/"))
 print("Exercise_1:", my_str1)
 
 
-# Exercise_2. Option_1 Напишитие программу, которая добавляет 'ing' к словам
+# Exercise_2. Option_1. Напишитие программу, которая добавляет 'ing' к словам
 
+list_1 = my_str1.split(".")
+print("Exercise_2_list_1:", type(list_1), list_1)
+res_1 = list_1[0] + "ing"
+res_2 = list_1[1] + "ing"
 
-words = my_str1.split(".")
-print("Exercise_2_words:", type(words), words)
-res_1 = words[0] + "ing"
-res_2 = words[1] + "ing"
-
-words1 = words[-1].split("/")
-print("Exercise_2_words_1:",type(words1), words1)
-res_3 = words1[0] + "ing"
-res_4 = words1[1] + "ing"
+list_2 = list_1[-1].split("/")
+print("Exercise_2_list_2:",type(list_2), list_2)
+res_3 = list_2[0] + "ing"
+res_4 = list_2[1] + "ing"
 print("Exercise_2_option_1:", res_1, res_2, res_3, res_4)
 
 
-# Exercise_2.Option_2 Напишитие программу, которая добавляет 'ing' к словам
+# Exercise_2. Option_2. Напишитие программу, которая добавляет 'ing' к словам
 
-
-string_2_2 = "try to do this exercise"
-split_1 = string_2_2.split(" ")
+list_2_2 = "try to do this exercise"
+split_1 = list_2_2.split(" ")
 print("Exercise_2_option_2:", type(split_1), split_1)
 add_1 = split_1[0] + "ing"
 add_2 = split_1[1] + "ing"
@@ -35,20 +32,24 @@ add_5 = split_1[4] + "ing"
 print("Exercise_2_option_2:", add_1, add_2, add_3, add_4, add_5)
 
 
+# Exercise_2. Option_3. Напишитие программу, которая добавляет 'ing' к слову
+
+str_2_3 = "start"
+print(f"Exercise_2_option_3:", (str_2_3) + "ing")
+
+
 # Exercise_3. В строке "Ivanou Ivan" поменяйте местами слова => "Ivan Ivanou"
 
-
-last_name = "Ivanou"
-first_name = "Ivan"
-str_3 = last_name + first_name
-print("Exercise_3:",type(str_3), str_3)
-
-str_1 = "Ivanou Ivan"
-print("Exercise_3:",type(str_1), str_1)
+my_list = "Ivanou Ivan"
+print("Exercise_3:", type(my_list), my_list)
+my_list_1 = my_list.split(" ")
+print("Exercise_3:", type(my_list_1), my_list_1)
+last_name = my_list_1[0]
+first_name = my_list_1[1]
+print("Exercise_3:", first_name + " " + last_name)
 
 
 # Exercise_4. Напишите программу, которая удаляет пробел в начале и конце строки
-
 
 str_4 = " Exercise 4 "
 print("Exercise_4:", str_4.startswith(" "))
