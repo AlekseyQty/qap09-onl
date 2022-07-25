61c3107b8b36c13ca49bbdb2f90d351f9d40ff6d
metalveor <metalveor@gmail.com>
2022-07-25 18:54:45 +0300

    Add_edit home_work_3.py

--- vajda_maksim_valerevich/hm_3/home_work_3.py
+++ vajda_maksim_valerevich/hm_3/home_work_3.py
@@ -2,18 +2,20 @@
 
 add_str = 'www.my_site.com#about'
 
-print('task_1: ' + add_str.replace('#', '/'))
-
+print('task_1/1: ' + add_str.replace('#', '/'))
+print(f'task_1/2: { add_str.replace("#", "/") }')
 
 # 2. Напишите программу, которая добавляет ‘ing’ к словам
 
+word = input('Word please:')
 word_1 = 'play'
 word_2 = 'drink'
 word_3 = 'jump'
 
 add_ing = 'ing'
 
-print('task_2: ' + word_1 + add_ing, word_2 + add_ing, word_3 + add_ing)
+print('task_2/1: ' + word_1 + add_ing, word_2 + add_ing, word_3 + add_ing)
+print(f'task_2/2: {word}ing')
 
 
 # 3. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
@@ -23,7 +25,9 @@ name = 'Ivanou Ivan'
 first_name = name[name.find(' ')+1:]
 last_name = name[:name.find(' ')+1]
 
-print('task_3: ' + first_name + ' ' + last_name)
+print('task_3/1: ' + first_name + ' ' + last_name)
+print('task_3/2: ' + ' '.join(name.split()[::-1]))
+
 
 # 4. Напишите программу которая удаляет пробел в начале, в конце строки
 
@@ -34,4 +38,8 @@ origin_str = '_The_world_under_spam_attack_'
 first_space = origin_str.find('_')
 last_space = origin_str.rfind('_')
 
-print('task_4: ' + origin_str[first_space+1:last_space])
+print('task_4/1: ' + origin_str[first_space+1:last_space])
+print('task_4/2: ' + origin_str.strip('_'))
+
+origin_str2 = input('Str please:')
+print('task_4/3: ' + origin_str2.strip())
