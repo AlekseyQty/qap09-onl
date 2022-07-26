2e1a17fe9e091bbac2dcb803be96f38041fb6f2a
fadeev denis <xsmugxjoex@yandex.ru>
2022-07-26 18:37:47 +0300

    Homework 3

--- fadeev_denis_olegovich/hm_3/homework_3_1.py
+++ fadeev_denis_olegovich/hm_3/homework_3_1.py
@@ -1,1 +1,4 @@
-#
+# Заменить символ "#" на символ "/" в строке 'www.my_site.com#about'
+site = "www.my_site.com#about"
+character_replacement = "/"
+print(site[:15] + character_replacement + site[15+1:])
--- fadeev_denis_olegovich/hm_3/homework_3_3.py
+++ fadeev_denis_olegovich/hm_3/homework_3_3.py
@@ -1,1 +1,3 @@
-#
+# В строке "Ivanou Ivan" поменяйте местами слова => "Ivan Ivanou"
+name = "Ivanou Ivan"
+print(name[7:], name[:7])
