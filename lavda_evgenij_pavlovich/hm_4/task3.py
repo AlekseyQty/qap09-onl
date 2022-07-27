4efe3c456d23e000ce3deceb45c0fa21e43861ef
Evgeniy Lavda <evgenlav1993@gmail.com>
2022-07-27 18:53:17 +0300

    homework4 done

---
+++ lavda_evgenij_pavlovich/hm_4/task3.py
@@ -0,0 +1,6 @@
+"""Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него
+строку => "I love arrays they are my favorite"""
+
+my_list = ["I", "love", "arrays", "they", "are", "my", "favorite"]
+my_string = " ".join(my_list)
+print(my_string)
