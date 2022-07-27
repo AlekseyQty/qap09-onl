816daf60c6b40e670bb23655c203e3572fbe309f
Boris Talataj <boristalataj@gmail.com>
2022-07-27 13:02:41 +0300

    homework3

---
+++ talataj_boris_gennadevich/hm3/homework3.py
@@ -0,0 +1,19 @@
+# Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+my_str = 'www.my_site.com#about'
+a = my_str.replace('#', '/')
+print(a)
+
+#Напишите программу, которая добавляет ‘ing’ к словам
+s_1 = 'last'
+s_2 = 'ing'
+print(s_1 + s_2)
+
+#В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+s = 'Ivanov Ivan'
+result = ' '.join(s.split(' ')[::-1])
+print(result)
+
+#Напишите программу которая удаляет пробел в начале, в конце строки
+name = ' My name is Boris '
+name.strip()
+print(name.strip())
