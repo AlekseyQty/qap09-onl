49cab65848013662e01c9af8c210718d7b23e445
AmelchenkoDima <Amelchencko.dima1998@gmail.com>
2022-07-31 16:57:01 +0300

    updated task 5

--- amelchenko_dmitrij_olegovich/hw4/hw_4.py
+++ amelchenko_dmitrij_olegovich/hw4/hw_4.py
@@ -39,8 +39,8 @@ print('Task 4: ',list_task_4)
 #5. 2 словаря необходимо их объединить по ключам, а значения ключей поместить в список,если у одного словаря есть ключ "а",
 # а у другого нету, то поставить значение None на соответствующую позицию
 
-a = { 'a': 1, 'b': 2, 'c': 3}
-b = { 'c': 3, 'd': 4,'e': 5}
+a = {'a': 1, 'b': 2, 'c': 3}
+b = {'c': 3, 'd': 4, 'e': 5}
 
 sotr_dict = sorted(list(set(list(a.keys()) + list(b.keys()))))
 
@@ -49,4 +49,4 @@ uni_keys = {}
 for key in sotr_dict:
     uni_keys[key] = [a.get(key), b.get(key)]
 
-print('Task 5: ',uni_keys)
+print('Task 5: ', uni_keys)
