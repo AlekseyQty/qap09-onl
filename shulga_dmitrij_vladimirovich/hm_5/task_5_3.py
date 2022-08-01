44cba197132ccf6555a7676da0d0f76fb9683a1b
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2022-08-01 12:49:22 +0300

    Add files to "Homework 5"

--- shulga_dmitrij_vladimirovich/hm_5/task_5_3.py
+++ shulga_dmitrij_vladimirovich/hm_5/task_5_3.py
@@ -8,55 +8,67 @@
 # При игре против компьютера игрок вводит комбинации одну за другой, пока не отгадает всю последовательность.
 # Ваша задача реализовать программу, против которой можно сыграть в "Быки и коровы"
 
+import random
+number = random.sample("1234567890", 4)
+number = int("".join(number))
+if number < 1000:
+    number = number * 10
+number = str(number)
+print(f" Hidden number: {number}")
+
 my_number = ""
 while my_number != "quit":
-    number = input("Please enter hidden four-digit number: ")
     my_number = input("Please enter your four-digit number: ")
-    #number = "1324"
     if my_number == number:
         print("Вы выиграли!")
         break
     else:
-        count = 0
-        for j in number:
-            if j in my_number:
-                count += 1
-            if count == 1:
-                count_1 = str(count)
-                count_1 = "Одна корова"
-                continue
-            if count == 2:
-                count_1 = str(count)
-                count_1 = "Две коровы"
-                continue
-            if count == 3:
-                count_1 = str(count)
-                count_1 = "Три коровы"
-                continue
-            if count == 4:
-                count_1 = str(count)
-                count_1 = "Четыри коровы"
-                continue
         count_2 = 0
-        for i in range(1, len(number)):
+        for i in range(len(number)):
             if number[i] == my_number[i]:
                 count_2 += 1
+                index = my_number[i]
             if count_2 == 1:
-                count_3 = str(count_2)
                 count_3 = "Один бык"
                 continue
             if count_2 == 2:
-                count_3 = str(count_2)
                 count_3 = "Два быка"
                 continue
-            if count_2 == 2:
-                count_3 = str(count_2)
+            if count_2 == 3:
                 count_3 = "Три быка"
+                continue
             if count_2 == 2:
-                count_3 = str(count_2)
                 count_3 = "Четыри быка"
+                continue
+            else:
+                count_3 = None
+                index = None
 
-        print(f"{count_1}, {count_3} ")
+        count = 0
+        for j in number:
+            if j in my_number:
+                if j == index:
+                    count_1 = ""
+                else:
+                    count += 1
+            if count == 1:
+                count_1 = "Одна корова"
+                continue
+            if count == 2:
+                count_1 = "Две коровы"
+                continue
+            if count == 3:
+                count_1 = "Три коровы"
+                continue
+            if count == 4:
+                count_1 = "Четыри коровы"
+                continue
+            else:
+                count_1 = None
 
+        if count_1 == count_3 == None:
+            print("Try again")
+        else:
+            print(f"{count_1} {count_3}")
 
 
