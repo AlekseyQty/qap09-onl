07b7122ba66128d2585e14fbaba7696150966e5e
metalveor <metalveor@gmail.com>
2022-08-03 22:27:31 +0300

    Add homework_6 task 3.py

---
+++ vajda_maksim_valerevich/hm_6/homework_6_3.py
@@ -0,0 +1,54 @@
+print(f'{"̅"*26}{"̅"*26}')
+print(f'{" "*20} THIRD TASK {" "*20}')
+print(f'{" "*20} CALCULATOR {" "*20}')
+print(f'{"̅"*26}{"̅"*26}')
+"""
+3. Простейший калькулятор v0.1
+Реализуйте программу, которая спрашивала у пользователя, какую операцию он хочет
+произвести над числами, а затем запрашивает два числа и выводит результат
+Проверка деления на 0 Реализовать программу нужно с использованием функций.
+Пример
+Выберите операцию:
+1 Сложение
+2 Вычитание
+3 Умножение
+4 Деление
+Введите номер пункта меню:
+>>> 4
+Введите первое число:
+>>> 10
+Введите второе число:
+>>> 3
+Частное: 3, Остаток: 3
+"""
+calc_start = int(input('Выберите операцию:\n'
+                                        '1. Сложение\n'
+                                        '2. Вычитание\n'
+                                        '3. Умножение\n'
+                                        '4. Деление\n'
+                                        'Введите номер пункта меню: '))
+
+if calc_start > 4:
+    print('Такой операции не существует, приходите позже')
+    print(f'{"̅" * 26}{"̅" * 26}')
+    quit()
+
+first_input = int(input('Введите первое число: '))
+second_input = int(input('Введите второе число: '))
+
+def calculator(first_input, second_input):
+    if calc_start == 1:
+        result = first_input + second_input
+    elif calc_start == 2:
+        result = first_input - second_input
+    elif calc_start == 3:
+        result = first_input * second_input
+    elif calc_start == 4:
+        if second_input == 0:
+            result = '\nОшибка: на < 0 > делить нелья'
+        else:
+            result = (f'\nЧастное : {int(first_input // second_input)}, Остаток: {int(first_input % second_input)}')
+    return (f'Результат вашей операции: {result}')
+
+print(calculator(first_input, second_input))
+print(f'{"̅"*26}{"̅"*26}')
