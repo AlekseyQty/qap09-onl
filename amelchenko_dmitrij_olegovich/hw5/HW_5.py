58bbf3a78a7a9eda1b809b570819ab733b5e30bc
AmelchenkoDima <Amelchencko.dima1998@gmail.com>
2022-08-13 21:22:52 +0300

    corrected task 2,3

--- amelchenko_dmitrij_olegovich/hw5/HW_5.py
+++ amelchenko_dmitrij_olegovich/hw5/HW_5.py
@@ -12,88 +12,92 @@ for i in range(2, a+1):
 #2.BuzzFuzz
 
 for i in range(1,101):
-    if i % 3 == 0:
-        print('Fuzz')
-    elif i % 5 == 0:
-        print('Buzz')
-    elif i % 3 == 0 and i % 5 == 0:
-        print('FuzzBuzz')
-    else:
-        i % 3 != 0 and i % 5 != 0
-        print(i)
+    if i % 3 == 0 and i % 5 != 0:
+        i = 'Fuzz'
 
-#3.Быки и коровы
+    elif i % 3 != 0 and i % 5 == 0:
+        i = 'Buzz'
+
+    elif i % 3 == 0 and i % 5 == 0:
+        i = 'FuzzBuzz'
+
+    print(i)
+
+# 3.Быки и коровы
 
 print('Игрок № 1, ввдите число из 4х разных цифр')
 
 while True:
     player_1 = input('=> :')
+    a = True
 
     if len(player_1) != 4:
         print('Игрок № 1, число должно состаять из 4х цифр!')
-        player_1
+        continue
 
-    else:
-        if len(str(player_1)) == 4:
+    for i in range(len(player_1) - 1):
+        for j in range(i + 1, len(player_1)):
+            if player_1[j] == player_1[i]:
+                print('Игрок № 1, число должно содержать 4 разных цифры!')
+                a = False
+                break
 
-                for i in range(len(player_1) - 1):
-                    for j in range(i + 1, len(player_1)):
-                        if player_1[j] == player_1[i]:
-                            print('Игрок № 1, число должно содержать 4 разных цифры!')
-                            player_1 = input('=> :')
-                        else:
-                            if player_1[j] != player_1[i]:
-                                continue
-    break
-
-print('Игрок № 2, ввдите число из 4х разных цифр')
-
-while True:
-    player_2 = input('=> :')
-
-    if len(player_2) != 4:
-        print('Игрок № 2, число должно состаять из 4х цифр!')
-        player_2
-
-    else:
-        if len(str(player_2)) == 4:
-
-                for i in range(len(player_2) - 1):
-                    for j in range(i + 1, len(player_2)):
-                        if player_2[j] == player_2[i]:
-                            print('Игрок № 2, число должно содержать 4 разных цифры!')
-                            player_2 = input('=> :')
-                        else:
-                            if player_2[j] != player_2[i]:
-                                continue
-    break
-
-player_1 = list(player_1)
-player_2 = list(player_2)
-
-bull = 0
-cow = 0
-
-while True:
-
-    for i in range(4):
-        if player_2[i] == player_1[i]:
-            bull += 1
-            player_1[i] = '-'
-            player_2[i] = '+'
-
-    if bull == 4:
-        print('Игрок № 2, Вы победили!!!')
+    if a == True:
+        player_1 = list(player_1)
         break
 
-    for i in range(4):
-        n = player_2[i]
-        for j in range(4):
-            if n == player_1[j]:
-                cow += 1
-                player_1[j] = '+'
-    break
+
+def play_2():
+    bull = 0
+    cow = 0
+
+    print('Игрок № 2, ввдите число из 4х разных цифр')
+
+    while True:
+
+        player_2 = input('=> :')
+        a = True
+
+        if len(player_2) != 4:
+            print('Игрок № 2, число должно состаять из 4х цифр!')
+            continue
+
+        for i in range(len(player_2) - 1):
+            for j in range(i + 1, len(player_2)):
+                if player_2[j] == player_2[i]:
+                    print('Игрок № 2, число должно содержать 4 разных цифры!')
+                    a = False
+                    break
+
+        if a == True:
+            player_2 = list(player_2)
+            break
+
+    while True:
+        for i in range(4):
+            if player_2[i] == player_1[i]:
+                bull += 1
+                player_1[i] = '-'
+                player_2[i] = '+'
+                continue
+
+        for i in range(4):
+            n = player_2[i]
+            for j in range(4):
+                if n == player_1[j]:
+                    cow += 1
+                    player_1[j] = '+'
+                    continue
+
+        if bull != 4:
+            print(f'Быков: {str(bull)}, Коров: {str(cow)}')
+            play_2()
+
+        else:
+            if bull == 4:
+                print(f'Вы выиграли!!! \n'
+                      f'Быки: {str(bull)} из 4')
+                break
 
 
-
-
+play_2()
