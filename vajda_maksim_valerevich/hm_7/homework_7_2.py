ab071a4a7bcfa0853ab440fbbf674e3874ba28ee
metalveor <metalveor@gmail.com>
2022-08-15 18:19:45 +0300

    added second generator to homework_7_2.py

--- vajda_maksim_valerevich/hm_7/homework_7_2.py
+++ vajda_maksim_valerevich/hm_7/homework_7_2.py
@@ -3,8 +3,9 @@ print(f'{" " * 20} SECOND TASK {" " * 20}')
 print(f'{" " * 20}  GENERATOR  {" " * 20}')
 print(f'{"̅" * 26}{"̅" * 26}')
 
-"""Генераторы:
-2.	Напишите генератор который принимает список 
+"""
+Генераторы:
+1.	Напишите генератор который принимает список 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7] 
 и возвращает новый список только с положительными числами
 """
@@ -20,4 +21,24 @@ def generator_positive(numbers):
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 for i in generator_positive(numbers):
-    print(i)
+    print(f'Generator 1: {i}')
+
+
+"""
+2.	Необходимо составить список чисел которые указывают на длину слов в строке: 
+sentence = " the quick brown fox jumps over the lazy dog", но только если слово не "the".
+"""
+
+
+def len_generator(sentence):
+    sentence_list = sentence.split()
+    new_list = []
+    for word in sentence_list:
+        if word == 'the':
+            continue
+        else:
+            new_list.append(len(word))
+    return f'Generator 2: {new_list}'
+
+
+print(len_generator(sentence=" the quick brown fox jumps over the lazy dog"))
