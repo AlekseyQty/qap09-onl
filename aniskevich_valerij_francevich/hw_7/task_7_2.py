d738824dc878065ffb3c014eb506aaaf26f26c70
Valery Aniskevich <aniskevich-2018@mail.ru>
2022-08-28 19:53:08 +0300

    Homework7: Aniskevich Valerij

--- aniskevich_valerij_francevich/hw_7/task_7_2.py
+++ aniskevich_valerij_francevich/hw_7/task_7_2.py
@@ -1,12 +1,16 @@
 # 2 Напишите генератор который принимает список numbers = [34.6, -203.4, 44.9, 68.3, -
 # 12.2, 44.6, 12.7] и возвращает новый список только с положительными числами
 
-numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+
+def generator(numbers):
+    my_list = []
+    for i in numbers:
+        if i > 0:
+            my_list.append(i)
+    yield my_list
 
 
-def func(a):
-    return a >= 0
+result = generator([34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7])
 
-
-new_number = list(filter(func, numbers))
-print(new_number)
+for i in result:
+    print(i)
