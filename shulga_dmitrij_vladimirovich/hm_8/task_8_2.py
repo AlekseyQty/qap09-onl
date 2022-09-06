cb4ec88a02fb40b206d2aa757bb6c6b619ad006d
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2022-09-06 19:17:02 +0300

    Changed file to "Homework 8"

--- shulga_dmitrij_vladimirovich/hm_8/task_8_2.py
+++ shulga_dmitrij_vladimirovich/hm_8/task_8_2.py
@@ -4,41 +4,42 @@
 
 import time
 
+
+def timeit(func):
+    def wrapper(time_delay):
+        start_1 = time.time()
+        func(0)
+        end_1 = time.time()
+
+        print(f" Time work: {round(end_1 - start_1, 4)}")
+
+        start_2 = time.time()
+        func(time_delay)
+        end_2 = time.time()
+
+        print(f" Time work after sleep: {round(end_2 - start_2, 4)}")
+
+    return wrapper
+
+@timeit
+def my_func(time_delay):
+    number = 10000
+    factorial = 1
+    for i in range(1, number + 1):
+        if i > 1:
+            factorial *= i
+        else:
+            factorial = 1
+    time.sleep(time_delay)
+
+
 delay = ""
 while delay != "quit":
-	delay = input("Please enter your time sleep: ")
+    delay = input("Please enter your time sleep: ")
 
-	if delay == "quit":
-		print("Goodbye")
-	else:
-		delay = int(delay)
-		def timeit(func):
+    if delay == "quit":
+        print("Goodbye")
+    else:
+        delay = int(delay)
 
-			def wrapper(delay):
-
-				func(0)
-				print(f" Time work: {round(time.time(), 2)}")
-				func(delay)
-				print(f" Time work: {round(time.time(), 2)}")
-
-			return wrapper
-
-		@timeit
-		def my_func(delay):
-
-			number = 3
-			factorial = 1
-			for i in range(1, number + 1):
-				if i > 1:
-					factorial *= i
-				else:
-					factorial = 1
-			print(f" Factorila: {factorial}")
-			time.sleep(delay)
-
-
-		my_func(delay)
-
-
-
-
+        my_func(delay)
