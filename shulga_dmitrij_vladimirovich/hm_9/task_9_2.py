483304377c26afb442002a2faf2db17fed8242c2
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2022-09-07 13:53:56 +0300

    Changed fileы to "Homework 9"

--- shulga_dmitrij_vladimirovich/hm_9/task_9_2.py
+++ shulga_dmitrij_vladimirovich/hm_9/task_9_2.py
@@ -3,24 +3,33 @@
 # капитализации - это означает, что проценты прибавляются к сумме инвестиции ежемесячно).
 # Написать класс Bank, метод deposit принимает аргументы N и R, и возвращает сумму, которая будет на счету пользователя.
 
-class Bank:
+class Investment:
 
     def __init__(self, sum, term):
         self.sum = sum
         self.term = term
 
+
+class Bank:
+    def __init__(self, invest):
+        self.invest = invest
+
     def deposit(self):
+
         result = 0
-        for i in range(self.term * 12):
-            self.sum = (self.sum * 0.1)/12 + self.sum
-            result = self.sum
+        for i in range(self.invest.term * 12):
+            self.invest.sum = (self.invest.sum * 0.1)/12 + self.invest.sum
+            result = self.invest.sum
         print(f"Result: {round(result, 2)}")
 
-sum = input("Please enter your sum: ")
-sum = int(sum)
-term = input("Please enter your term in years: ")
-term = int(term)
 
-my_deposit = Bank(sum, term)
-my_deposit.deposit()
+my_sum = input("Please enter your sum: ")
+my_sum = int(my_sum)
+my_term = input("Please enter your term in years: ")
+my_term = int(my_term)
+
+my_deposit = Investment(my_sum, my_term)
+my_bank = Bank(my_deposit)
+my_bank.deposit()
+
 
